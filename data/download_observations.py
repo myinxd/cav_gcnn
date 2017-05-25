ffbbe401c71e8e30657034346b641fd214f222fc
Zhixian Ma <zxma_sjtu@qq.com>
2017-05-25 09:04:10 +0800

    Add a script to download the target observations.

---
+++ data/download_observations.py
@@ -0,0 +1,63 @@
+# Copyright (C) 2016 Zhixian MA <zxma_sjtu@qq.com>
+
+"""
+Fetch observations from chandra
+
+Reference
+--------
+    [1] http://cxc.cfa.harvard.edu/ciao/ahelp/download_chandra_obsid.html
+    [2] http://cxc.cfa.harvard.edu/ciao/ahelp/find_chandra_obsid.html
+
+"""
+
+import os
+import pandas as pd
+
+
+def main():
+    """
+    Fetch observations from chandra
+    """
+    # Init
+    objname = 'objlist.txt'
+    obsidname = 'obsid.txt'
+    csv_folder = 'csv'
+    # path_all = 'AllSamples'
+
+    # handles
+    f = open(objname, 'r')
+    fo = open(obsidname, 'w+')
+
+    if not os.path.exists(csv_folder):
+        os.mkdir(csv_folder)
+    for sample in f:
+        sample = sample[:-1]
+        sample = sample.replace(' ', '_')
+        print("Sample name: %s" % sample)
+        # Get obsid table
+        csv_name = ("%s.csv") % sample
+        csv_path = os.path.join(csv_folder, csv_name)
+        print("find_chandra_obsid %s > %s" % (sample, csv_path))
+        os.system("find_chandra_obsid %s > %s" % (sample, csv_path))
+        # get obsid
+        print("dmlist '%s[cols obsid]' data,raw > temp.csv" % (csv_path))
+        os.system("dmlist '%s[cols obsid]' data,raw > temp.csv" % (csv_path))
+        sp_table = pd.read_csv('temp.csv')
+        obs_line = sp_table.values[-1, 0]
+        obsid = str(int(obs_line))
+        fo.write('%s\t' % obsid)
+        # download all files
+        print("download_chandra_obsid %s" % obsid)
+        os.system("download_chandra_obsid %s" % obsid)
+        # repro
+        print("chandra_repro %s/. %s/repro" % (obsid, obsid))
+        os.system("chandra_repro %s/. %s/repro" % (obsid, obsid))
+        # change folder name
+        print("mv %s %s" % (obsid, sample))
+        os.system("mv %s %s" % (obsid, sample))
+
+    f.close()
+
+
+if __name__ == "__main__":
+    main()
--- sample/README.md
+++
@@ -1,8 +0,0 @@
-## Description of the dataset
-The X-ray observations used in our work are listed in this folder, which are transformed into jpegs, and marked with corresponding ObsID (Observation ID) and scales (See as follows). The snap of the selected samples marked with cavities is illustrated in [samples_cav.png](https://github.com/myinxd/cav_gcnn/blob/master/sample/samples_cav.png)
-
-To fetch these observations, a python scipt namely [getSamples.py](https://github.com/myinxd/cavdet/blob/master/utils/getSamples.py) is provided, please enjoy yourself to build your own datasets. It should be noticed that a list file namely `objlist.txt` is required to apply our script, as well as the Chandra software [`CIAO`](http://cxc.harvard.edu/ciao/). 
-
-<center>
-<img src="https://github.com/myinxd/cav_gcnn/blob/master/sample/samples.png" height=1350 width=850/>
-</center>
